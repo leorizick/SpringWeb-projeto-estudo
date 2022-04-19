f6d2a6a1bf983e64f96e2df62f648c6e7e93a44c
Leonardo Rizick <leorizick@hotmail.com>
2022-04-19 13:41:01 -0300

    Primeiro commit, Rest e categoria criado

---
+++ src/main/java/com/leorizick/SpringWeb/resources/CategoryResources.java
@@ -0,0 +1,26 @@
+package com.leorizick.SpringWeb.resources;
+
+import com.leorizick.SpringWeb.domain.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categoria")
+public class CategoryResources {
+
+    @GetMapping
+    public List<Category> list(){
+
+        List<Category> list = new ArrayList<>();
+        Category cat1 = new Category(1, "Informatica");
+        Category cat2 = new Category(2, "Eletronicos");
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
+    }
+}
---
+++ src/test/java/com/leorizick/SpringWeb/SpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leorizick.SpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
