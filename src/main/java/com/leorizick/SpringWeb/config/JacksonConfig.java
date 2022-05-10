56a59be541fcf83016a1b55918fc338384f132cb
Leonardo Rizick <leorizick@hotmail.com>
2022-05-10 10:09:47 -0300

    Configuracao inicial do Spring Security

--- src/main/java/com/leorizick/SpringWeb/config/JacksonConfig.java
+++ src/main/java/com/leorizick/SpringWeb/config/JacksonConfig.java
@@ -6,6 +6,8 @@ import com.leorizick.SpringWeb.domain.PagamentoComCartao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class JacksonConfig {
@@ -17,6 +19,11 @@ public class JacksonConfig {
             };
         };
         return builder;
+    }
+
+    @Bean
+    public JavaMailSender jMS (){
+        return new JavaMailSenderImpl();
     }
 }
 
